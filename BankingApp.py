407ebc9adf4d71cf221ddfc3ccd443b8a7e96171
Adrian <adrian.george.radu@gmail.com>
2023-03-17 15:20:49 +0100

    Adrians one

--- BankingApp.py
+++ BankingApp.py
@@ -2,8 +2,17 @@
 
 
 class BankAccount:
-    pass
+    def __init__(self, iban:str, total=0, status='white'):
+        self.iban     = iban
+        self.total    = total
+        self.status   = status
+        self.deposits = [] # list of dictionaries like {'title':'Blabla','value':1234,'date':'17.03.2023'}
+        self.withdraws = [] # list of dictionaries like {'title':'Blabla','value':1234,'date':'17.03.2023'}
 
+    def turn_off(self):
+        self.turned_on = False
+        print("The TV is off.")
+        print()
 
 
 # Maksym
