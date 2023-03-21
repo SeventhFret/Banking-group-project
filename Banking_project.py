23538d9a34f43b1a73b3269c959e9c83ccf8ffb5
Jason24 <y.r.ukrainianboy@icloud.com>
2023-03-21 09:49:28 +0100

    mars

--- Banking_project.py
+++ Banking_project.py
@@ -1,3 +1,6 @@
-class Bank:
-    def __init__(self):
-        self.accounts = []
+def deposit(self):
+    title = input("Description of your deposit: ")
+    value = int(input("How much money would you like to deposit? "))
+    transaction_date = datetime.datetime.now()
+    self.balance += value
+    self.transactions.append({'title': title, 'value': value, 'date': transaction_date})
